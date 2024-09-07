b456af0a10c64e1b02b7371074889dce79933222
sami855-ux <samitale56@outlook.com>
2024-09-07 13:41:40 +0500

    Reciving the data from fake API

--- src/Pages/Homepage.jsx
+++ src/Pages/Homepage.jsx
@@ -1,9 +1,34 @@
+import { useEffect, useState } from "react"
+
 import Header from "../Component/Header"
+import styles from "./Homepage.module.css"
+
+const BASE_URL = "http://localhost:1234/item"
 
 function Homepage() {
+  const [itemData, setItemData] = useState([])
+  const [isLoading, setIsLoading] = useState(false)
+
+  useEffect(() => {
+    setIsLoading(true)
+    async function fetchData() {
+      try {
+        const res = await fetch(`${BASE_URL}`)
+        const data = await res.json()
+
+        setItemData(data)
+      } catch (error) {
+        console.log(error.message)
+      } finally {
+        setIsLoading(false)
+      }
+    }
+    fetchData()
+  }, [])
   return (
     <>
       <Header />
+      <main className={styles.mainWrapper}></main>
     </>
   )
 }
