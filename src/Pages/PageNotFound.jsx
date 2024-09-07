27464e9f7efa35f0c129acc5692042d7c8832ffe
sami855-ux <samitale56@outlook.com>
2024-09-07 13:29:15 +0500

    Page not found page is added

---
+++ src/Pages/PageNotFound.jsx
@@ -0,0 +1,12 @@
+import Header from "../Component/Header"
+
+function PageNotFound() {
+  return (
+    <>
+      <Header />
+      <h3>Page Not Found 😞</h3>
+    </>
+  )
+}
+
+export default PageNotFound
