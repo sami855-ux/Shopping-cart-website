3b880e90b1422b5583bf98f0a39f9e405845b5bf
sami855-ux <samitale56@outlook.com>
2024-09-07 13:01:00 +0500

    Defining the Routes for the page

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,14 @@
-import { BrowserRouter, Route, Router } from "react-router-dom"
+import { BrowserRouter, Route, Routes } from "react-router-dom"
+import Homepage from "./Pages/Homepage"
+import Cart from "./Pages/Cart"
 
 function App() {
   return (
     <BrowserRouter>
-      <Router></Router>
+      <Routes>
+        <Route index element={<Homepage />} />
+        <Route path="cart" element={<Cart />} />
+      </Routes>
     </BrowserRouter>
   )
 }
